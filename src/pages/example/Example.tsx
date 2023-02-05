eefb7d83326de5c7099b0b33229aa9a9ff5de7f1
Adiz Abdullaev <adizabdullaev@Adizs-MBP.fritz.box>
2023-02-05 13:39:45 +0100

    fix: updated mpd source from s3

--- src/pages/example/Example.tsx
+++ src/pages/example/Example.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useParams, useNavigate, useNavigationType } from "react-router-dom";
 import { BasicSearchForm } from "../../components";
 import { Header, Footer } from "../../layout";
@@ -12,7 +13,27 @@ const Example = () => {
     navigate(-1);
   };
 
-  console.log("navigationType", navigationType);
+  useEffect(() => {
+    let player: unknown | undefined = undefined;
+    setTimeout(() => {
+      player = createPlayer();
+      const url = "http://mavira.ddns.net:9200/dash-sandbox/dash.mpd";
+      // @ts-ignore
+      player.initialize(document.querySelector("#videoPlayer"), url, true);
+    }, 1000);
+    return () => {
+      if (player) {
+        // @ts-ignore
+        player.destroy();
+      }
+    };
+  }, []);
+
+  const createPlayer = (): unknown => {
+    // @ts-ignore
+    const player = dashjs.MediaPlayer().create();
+    return player;
+  };
 
   return (
     <>
@@ -31,11 +52,12 @@ const Example = () => {
       <div className="height-70-vh mt-5">
         <div className="card mb-3">
           <video
+            id="videoPlayer"
             className="card-img-top"
             data-dashjs-player
-            src="/video/dash.mpd"
             controls
           ></video>
+
           <div className="card-body">
             <h5 className="card-title text-start">{mockExample.title}</h5>
             <p className="card-text text-start ">{mockExample.text}</p>
