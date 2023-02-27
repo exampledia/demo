2ea1f79709cd6c50c9d84edc37daeabe1e8da988
Adiz Abdullaev <adizabdullaev@Adizs-MBP.fritz.box>
2023-02-27 16:48:52 +0100

    feat: debug and prod modes for dash-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => {
+  const dashJSCDNPath = `https://cdn.dashjs.org/latest/dash.all.${mode === 'production' ? 'min' : 'debug'}.js`
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            dashJSCDNPath,
+          },
+        },
+      }),
+    ],
+  };
 })
