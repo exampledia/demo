ee80bda28838557d313deb7708b5356060a93b55
Carmelo <97826655+carmelodevuz@users.noreply.github.com>
2023-02-27 18:35:19 +0100

    fix: update dash mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-  const dashJSCDNPath = `https://cdn.dashjs.org/latest/dash.all.${mode === 'production' ? 'min' : 'debug'}.js`
+  const dashJSCDNPath = `https://cdn.dashjs.org/latest/dash.all.${mode === 'development' ? 'debug' : 'min'}.js`
   return {
     plugins: [
       react(),
